dc0bc1a8f91cfc318defdccb00fc4f9a4ef02f62
Snow <i@xkun.me>
2022-06-18 20:34:03 +0800

    time measuring and testing

--- main.cc
+++ main.cc
@@ -1,58 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "knn.h"
+#include "utils.h"
+
+#define N_NUM 8
+#define D_NUM 5
 
 int main(int argc, char *argv[])
 {
+    int k = 20;
+    int ns[N_NUM];
+    int ds[D_NUM];
+    for (int i = 0; i < N_NUM; i++)
+        ns[i] = 256 << i;
+    for (int j = 0; j < D_NUM; j++)
+        ds[j] = 1 << (j * 2);
 
-    float x[] = {
-        0.0f, 1.0f,
-        1.0f, 0.0f,
-        0.0f, 0.0f
-    };
+    long cuda_time[N_NUM][D_NUM];
+    long cublas_time[N_NUM][D_NUM];
+    long omp_time[N_NUM][D_NUM];
 
-    float y[] = {
-        1.0f, 1.0f,
-        1.0f, 2.0f,
-        0.5f, 0.5f,
-        -1.0f, 0.0f,
-        0.0f, -1.0f
-    };
+    float *x         = NULL;
+    float *y         = NULL;
+    int   *cuda_idx  = NULL;
+    float *cuda_dist = NULL;
+    int   *omp_idx   = NULL;
+    float *omp_dist  = NULL;
 
-    int m = 3;
-    int n = 5;
-    int d = 2;
-    int k = 2;
+    for (int i = 0; i < N_NUM; i++) {
+        for (int j = 0; j < D_NUM; j++) {
+            int m = ns[i];
+            int n = ns[i];
+            int d = ds[j];
 
-    int *knn_idx = (int *)malloc(sizeof(int) * m * k);
-    float *knn_dist = (float *)malloc(sizeof(float) * m * k);
+            x = (float *)malloc(sizeof(float) * m * d);
+            y = (float *)malloc(sizeof(float) * n * d);
 
-    knn_cuda(x, y, m, n, d, k, knn_idx, knn_dist);
+            // generate points randomly
+            generate_data(x, y, m, n, d);
 
-#ifdef DEBUG
-    for (int p = 0; p < m; p++) {
-        printf("%d-nearest neighbor of ", k);
-        printf("(");
-        for (int i = 0; i < d; i++) {
-            if (i == d - 1)
-                printf("%f)\n", x[p * d + i]);
-            else
-                printf("%f, ", x[p * d + i]);
-        }
-        for (int i = 0; i < k; i++) {
-            printf("(");
-            for (int j = 0; j < d; j++) {
-                if (j == d - 1)
-                    printf("%f): %f\n", y[knn_idx[p * k + i] * d + j], knn_dist[p * k + i]);
-                else
-                    printf("%f, ", y[knn_idx[p * k + i] * d + j]);
-            }
+            // allocate memory for storing the results
+            cuda_idx  = (int *)  malloc(sizeof(int)   * m * k);
+            cuda_dist = (float *)malloc(sizeof(float) * m * k);
+            omp_idx   = (int *)  malloc(sizeof(int)   * m * k);
+            omp_dist  = (float *)malloc(sizeof(float) * m * k);
+
+            long start_time, end_time;
+
+            // knn cuda version
+            start_time = get_time();
+            knn_cuda(x, y, m, n, d, k, cuda_idx, cuda_dist);
+            end_time = get_time();
+            printf("with configuration m = n = %d, d = %d, k = %d\n", m, d, k);
+            printf("  knn cuda version : %ld ms\n", end_time - start_time);
+            cuda_time[i][j] = end_time - start_time;
+
+            // knn cublas version
+            start_time = get_time();
+            knn_cublas(x, y, m, n, d, k, cuda_idx, cuda_dist);
+            end_time = get_time();
+            printf("  knn cublas version : %ld ms\n", end_time - start_time);
+            cublas_time[i][j] = end_time - start_time;
+
+            // knn openmp version
+            start_time = get_time();
+            knn_omp(x, y, m, n, d, k, omp_idx, omp_dist);
+            end_time = get_time();
+            printf("  knn omp version : %ld ms\n", end_time - start_time);
+            omp_time[i][j] = end_time - start_time;
+
+            error_check(cuda_idx, cuda_dist, omp_idx, omp_dist, m, k);
+
+            free(x);
+            free(y);
+            free(cuda_idx);
+            free(cuda_dist);
+            free(omp_idx);
+            free(omp_dist);
         }
     }
-#endif
 
-    free(knn_idx);
-    free(knn_dist);
+    FILE *fp;
+    fp = fopen("time.txt", "w");
+    for (int i = 0; i < N_NUM; i++) {
+        for (int j = 0; j < D_NUM; j++)
+            fprintf(fp, "%ld ", cuda_time[i][j]);
+        fprintf(fp, "\n");
+    }
+    for (int i = 0; i < N_NUM; i++) {
+        for (int j = 0; j < D_NUM; j++)
+            fprintf(fp, "%ld ", cublas_time[i][j]);
+        fprintf(fp, "\n");
+    }
+    for (int i = 0; i < N_NUM; i++) {
+        for (int j = 0; j < D_NUM; j++)
+            fprintf(fp, "%ld ", omp_time[i][j]);
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
 
     return 0;
 }
