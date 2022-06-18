ad55b9643e290cbcb2b14cce9222fb058423a4f4
‘liyle3’ <2649398275@qq.com>
2022-06-18 16:50:57 +0800

    utils: implement data generation and result output

--- utils.h
+++ utils.h
@@ -11,7 +11,7 @@
                 error, cudaGetErrorString(error));          \
         exit(1);                                            \
     }                                                       \
-}     
+}
 
 #define divup(a, b) ((a) % (b) == 0 ? ((a) / (b)) : ((a) / (b) + 1))
 
@@ -21,6 +21,66 @@ __global__ void warmup()
 {
     // warmup kernel
     // do nothing
+}
+
+
+/**
+ *  @brief randomly generate test data
+ *
+ *  @x: query points
+ *  @y: reference points
+ *  @m: number of query points
+ *  @n: number of reference points
+ *  @d: point dimension
+ *
+ */
+void generate_data(
+    float *x,     // query points
+    float *y,     // reference points
+    int m,        // #query points
+    int n,        // #reference points
+    int d         // point dimension
+)
+{
+    srand(time(NULL));
+    for (int i = 0; i < m * d; i++) {
+        x[i] = -500 + (1000.0f * rand()  / RAND_MAX );
+    }
+
+    for (int i = 0; i < n * d; i++) {
+        y[i] = -500 + (1000.0f * rand()  / RAND_MAX );
+    }
+
+}
+
+
+/**
+ * @brief
+ *
+ * @param filename
+ * @param m
+ * @param k
+ * @param indices
+ * @param distance
+ */
+void output(
+    const char *filename,
+    int m,
+    int k,
+    int *indices,
+    float *distance
+)
+{
+    FILE* file;
+    file = fopen(filename, "w");
+    if(file != NULL) {
+        for (int p = 0; p < m; p++) {
+            for (int i = 0; i < k; i++) {
+                fprintf(file, "%d %.5f", indices[p * k + i], distance[p * k + i]);
+            }
+        }
+    }
+    fclose(file);
 }
 
 #endif
